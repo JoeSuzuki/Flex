fc7804e0192a4f5ebf588b898bd02afba9ec1e9f
JoeSuzuki <joesuzuki516@gmail.com>
2017-12-31 02:50:52 -0500

    user and group models

---
+++ Flex/Flex/Models/Group.swift
@@ -0,0 +1,19 @@
+//
+//  Group.swift
+//  Flex
+//
+//  Created by Joe Suzuki on 12/31/17.
+//  Copyright © 2017 Joe Suzuki. All rights reserved.
+//
+
+import UIKit
+
+struct Group {
+    let ownerName: String
+    let groupName: String
+    let location: String
+    let startCloseTime: [Date: Date]
+    let openDay: [String]
+    let groupBio: String
+    let groupImage: UIImage
+}
---
+++ Flex/Flex/Models/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Flex
+//
+//  Created by Joe Suzuki on 12/31/17.
+//  Copyright © 2017 Joe Suzuki. All rights reserved.
+//
+
+import UIKit
+
+struct User {
+    let name: String
+    let username: String
+    let age: String
+    let sex: String
+    let email: String
+    let profileImageUrl: UIImage
+}
