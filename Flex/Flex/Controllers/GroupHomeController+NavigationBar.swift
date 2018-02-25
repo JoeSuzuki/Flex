5a7a1cf45b3567f2ac05829f03c67949e98039e7
JoeSuzuki <joesuzuki516@gmail.com>
2018-02-24 19:59:50 -0500

    navigation bar design

--- Flex/Flex/Controllers/GroupHomeController+NavigationBar.swift
+++ Flex/Flex/Controllers/GroupHomeController+NavigationBar.swift
@@ -17,12 +17,15 @@ extension GroupHomeController {
     }
     
     private func setupRemainNavItems() {
-        let tittleImageView = UIImageView(image: #imageLiteral(resourceName: "Flex").withRenderingMode(.alwaysTemplate))
-        tittleImageView.tintColor = .white
-        tittleImageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
-        tittleImageView.contentMode = .scaleAspectFit
-        navigationItem.titleView = tittleImageView
+//        let tittleImageView = UIImageView(image: #imageLiteral(resourceName: "Flex").withRenderingMode(.alwaysTemplate))
+//        tittleImageView.tintColor = .white
+//        tittleImageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+//        tittleImageView.contentMode = .scaleAspectFit
+//        navigationItem.titleView = tittleImageView
         
+        navigationItem.title = "Flex"
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 26),NSAttributedStringKey.foregroundColor: UIColor.white]
+
         navigationController?.navigationBar.barTintColor = UIColor.main.mainBlue
         navigationController?.navigationBar.isTranslucent = false
 
@@ -44,8 +47,8 @@ extension GroupHomeController {
     private func setupLeftNavItems() {
         let followButton = UIButton(type: .system)
         followButton.setImage(#imageLiteral(resourceName: "Clock").withRenderingMode(.alwaysOriginal), for: .normal)
-        followButton.widthAnchor.constraint(equalToConstant: 34).isActive = true
-        followButton.heightAnchor.constraint(equalToConstant: 34).isActive = true
+        followButton.widthAnchor.constraint(equalToConstant: 25).isActive = true
+        followButton.heightAnchor.constraint(equalToConstant: 25).isActive = true
 
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: followButton)
     }
@@ -53,8 +56,8 @@ extension GroupHomeController {
     private func setupRightNavItems() {
         let searchButton = UIButton(type: .system)
         searchButton.setImage(#imageLiteral(resourceName: "Home").withRenderingMode(.alwaysOriginal), for: .normal)
-        searchButton.widthAnchor.constraint(equalToConstant: 34).isActive = true
-        searchButton.heightAnchor.constraint(equalToConstant: 34).isActive = true
+        searchButton.widthAnchor.constraint(equalToConstant: 25).isActive = true
+        searchButton.heightAnchor.constraint(equalToConstant: 25).isActive = true
 
         navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: searchButton)]
     }
