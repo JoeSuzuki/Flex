de387275391a6eeddf947ff6797e4cb533065d75
JoeSuzuki <joesuzuki516@gmail.com>
2018-03-03 21:43:36 -0500

    setup settings/profile

--- Flex/Flex/Controllers/ProfileViewController.swift
+++ Flex/Flex/Controllers/ProfileViewController.swift
@@ -8,3 +8,9 @@
 
 import UIKit
 
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Flex/Flex/Controllers/SettingViewController.swift
+++ Flex/Flex/Controllers/SettingViewController.swift
@@ -7,3 +7,9 @@
 //
 
 import UIKit
+
+class SettingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
