db07c04137ac575b728fb8c2694ae0af37f953ab
JoeSuzuki <joesuzuki516@gmail.com>
2018-03-03 21:35:41 -0500

    profile + settings

---
+++ Flex/Flex/Controllers/ProfileViewController.swift
@@ -0,0 +1,10 @@
+//
+//  ProfileViewController.swift
+//  Flex
+//
+//  Created by Joe Suzuki on 3/3/18.
+//  Copyright © 2018 Joe Suzuki. All rights reserved.
+//
+
+import UIKit
+
---
+++ Flex/Flex/Controllers/SettingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingViewController.swift
+//  Flex
+//
+//  Created by Joe Suzuki on 3/3/18.
+//  Copyright © 2018 Joe Suzuki. All rights reserved.
+//
+
+import UIKit
