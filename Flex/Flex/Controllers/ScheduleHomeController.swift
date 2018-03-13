fe98d9b2df484fc185a1353f9e765d30f8552b9a
JoeSuzuki <joesuzuki516@gmail.com>
2018-03-12 20:17:42 -0400

    hard set time

--- Flex/Flex/Controllers/ScheduleHomeController.swift
+++ Flex/Flex/Controllers/ScheduleHomeController.swift
@@ -26,7 +26,7 @@ class ScheduleHomeController: UICollectionViewController,UICollectionViewDelegat
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 24
     }
-    
+
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
         return cell
--- Flex/Flex/Views/ScheduleCell.swift
+++ Flex/Flex/Views/ScheduleCell.swift
@@ -48,7 +48,9 @@ class ScheduleCell: UICollectionViewCell {
     }()
     let activityTimeLabel: UILabel = {
         let label = UILabel()
-        label.text = "12:00 PM"
+        let time: [String] = ["12:00 AM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM", "9:00 PM", "10:00 PM", "11:00 PM", "12:00 PM", "1:00 AM", "2:00 AM", "3:00 AM", "4:00 AM", "5:00 AM", "6:00 AM", "7:00 AM", "8:00 AM", "9:00 AM","10:00 AM", "11:00 AM"
+
+        label.text = "\(time[0])"
         label.textColor = UIColor.main.mainBlue
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
