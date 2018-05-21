d9b867473784402cf5aba12d32814081d5d6494d
JoeSuzuki <joesuzuki516@gmail.com>
2018-05-21 19:08:39 -0400

    Login view UI redesign

--- Flex/Flex/Controllers/LoginsViewController.swift
+++ Flex/Flex/Controllers/LoginsViewController.swift
@@ -24,28 +24,14 @@ class LoginsViewController: UIViewController {
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         button.layer.borderWidth = 1
         button.layer.borderColor = UIColor.mainBlue.cgColor
-        button.layer.cornerRadius = 10
+        button.layer.cornerRadius = 14
         button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
-        
-        return button
-    }()
-    let registerButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.backgroundColor = UIColor.white
-        button.setTitle("Register", for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitleColor(.mainBlue, for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
-        button.layer.borderWidth = 1
-        button.layer.borderColor = UIColor.mainBlue.cgColor
-        button.layer.cornerRadius = 10
-        button.addTarget(self, action: #selector(handleRegister), for: .touchUpInside)
         
         return button
     }()
     let containerView: UIView = {
         let view = UIView()
-        view.backgroundColor = .mainBlue
+        view.backgroundColor = .clear
         return view
     }()
     let logoImageView: UIImageView = {
@@ -63,7 +49,6 @@ class LoginsViewController: UIViewController {
         view.backgroundColor = .mainBlue
         
         view.addSubview(loginButton)
-        view.addSubview(registerButton)
         view.addSubview(containerView)
         view.addSubview(logoImageView)
 
@@ -71,13 +56,15 @@ class LoginsViewController: UIViewController {
         let height = view.layer.frame.height / 12
         let width = view.layer.frame.width
 
-        loginButton.anchor(top: containerView.bottomAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: registerButton.topAnchor, trailing: view.safeAreaLayoutGuide.trailingAnchor, size: .init(width: width, height: height))
-        registerButton.anchor(top: loginButton.bottomAnchor, leading: registerButton.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: registerButton.trailingAnchor, size: .init(width: width, height: height))
+        containerView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: loginButton.topAnchor, trailing: view.safeAreaLayoutGuide.trailingAnchor, size: .init(width: width, height: height))
+
+        loginButton.anchor(top: containerView.bottomAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 0, left: 10, bottom: 0, right: 10), size: .init(width: width, height: height))
         
-        containerView.anchor(top: nil, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: loginButton.topAnchor, trailing: view.safeAreaLayoutGuide.trailingAnchor)
-        
-        logoImageView.anchor(top: containerView.topAnchor, leading: containerView.leadingAnchor, bottom: containerView.bottomAnchor, trailing: containerView.trailingAnchor, size: .init(width: 50, height: 50))
+//        logoImageView.anchor(top: containerView.topAnchor, leading: containerView.leadingAnchor, bottom: containerView.bottomAnchor, trailing: containerView.trailingAnchor, size: .init(width: 50, height: 100))
     
+        logoImageView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor).isActive = true
+        logoImageView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        
 //        logoImageView.heightAnchor.constraint(equalToConstant: view.layer.frame.height / 10)
 
     }
