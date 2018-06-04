0d4cca614e43b9f6e7a877b447563815e10a3ccc
JoeSuzuki <joesuzuki516@gmail.com>
2018-06-03 23:26:39 -0400

    form customization

--- Flex/Flex/Controllers/CreateGroupFormController.swift
+++ Flex/Flex/Controllers/CreateGroupFormController.swift
@@ -7,6 +7,7 @@
 //
 
 import Eureka
+import ImageRow
 
 class CreateGroupFormController: FormViewController {
     
@@ -14,26 +15,50 @@ class CreateGroupFormController: FormViewController {
         super.viewDidLoad()
         setupNavigationBarItems()
         
-        form +++ Section("Section1")
-            <<< TextRow(){ row in
-                row.title = "Text Row"
-                row.placeholder = "Enter text here"
+        form +++ Section("Basic Info")
+            <<< NameRow(){ row in
+                row.title = "Name"
+                row.placeholder = "Enter your group name"
             }
-            <<< PhoneRow(){
-                $0.title = "Phone Row"
-                $0.placeholder = "And numbers here"
+            <<< ActionSheetRow<String>() {
+                $0.title = "Gender"
+                $0.selectorTitle = "Gender"
+                $0.options = ["Male","Female","Other"]
+                $0.value = "Male"
             }
-            +++ Section("Section2")
-            <<< DateRow(){
-                $0.title = "Date Row"
-                $0.value = Date(timeIntervalSinceReferenceDate: 0)
-        }
+            <<< MultipleSelectorRow<String>() {
+                $0.title = "Availability"
+                $0.selectorTitle = "Availability"
+                $0.options = ["Sunday", "Monday", "Tuesday", "Wensday", "Thursday", "Friday", "Saturday"]
+            }
+            
+            +++ Section("")
+            <<< ImageRow() {
+                $0.title = "Profile Picture"
+                $0.sourceTypes = [.PhotoLibrary, .SavedPhotosAlbum]
+                $0.clearAction = .yes(style: .default)
+                }
+                .cellUpdate { cell, row in
+                    cell.accessoryView?.layer.cornerRadius = 17
+                    cell.accessoryView?.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+            }
+
+            +++ Section("Contact Details")
+            <<< EmailRow(){
+                $0.title = "Email"
+                $0.placeholder = "Enter your email"
+            }
+            <<< PhoneRow() {
+                $0.title = "Phone Number"
+                $0.placeholder = "(###) ###-####"
+            }
         // Enables the navigation accessory and stops navigation when a disabled row is encountered
         navigationOptions = RowNavigationOptions.Enabled.union(.StopDisabledRow)
         // Enables smooth scrolling on navigation to off-screen rows
         animateScroll = true
     }
 }
+
 
 extension CreateGroupFormController {
     func setupNavigationBarItems() {
@@ -93,4 +118,3 @@ extension CreateGroupFormController {
     }
 
 }
-
