1ecf1a0862d753a3c3986c357930003b1794fe78
JoeSuzuki <joesuzuki516@gmail.com>
2018-07-07 23:07:24 -0400

    New cells for create group view

---
+++ Flex/Views/CreateGroupCells.swift
@@ -0,0 +1,82 @@
+//
+//  CreateGroupCells.swift
+//  Flex
+//
+//  Created by Joe Suzuki on 7/7/18.
+//  Copyright © 2018 Joe Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class GroupNameCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    func setupViews() {
+        backgroundColor = .white
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+class GroupDescriptionCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    func setupViews() {
+        backgroundColor = .white
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+class GroupLocationCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    func setupViews() {
+        backgroundColor = .white
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+class GroupAvailbilityCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    func setupViews() {
+        backgroundColor = .white
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+class GroupProfilePictureCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    func setupViews() {
+        backgroundColor = .white
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+class GroupExtraDetailCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    func setupViews() {
+        backgroundColor = .white
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
