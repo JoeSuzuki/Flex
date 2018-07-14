3fe6b891e54ad9dc2306ae0ec6d5c4413572292c
JoeSuzuki <joesuzuki516@gmail.com>
2018-07-13 22:59:42 -0400

    Removed Header cell

---
+++ Description/DescriptionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  DescriptionViewCell.swift
+//  Flex
+//
+//  Created by Joe Suzuki on 7/13/18.
+//  Copyright © 2018 Joe Suzuki. All rights reserved.
+//
+
+import Foundation
---
+++ DescriptionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  DescriptionViewCell.swift
+//  
+//
+//  Created by Joe Suzuki on 7/13/18.
+//
+
+import Foundation
