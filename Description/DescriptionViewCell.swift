0a6130d2288264104391251c86891efdd446c0f7
JoeSuzuki <joesuzuki516@gmail.com>
2018-07-14 18:42:36 -0400

    Completed View schedule button

--- Description/DescriptionViewCell.swift
+++ Description/DescriptionViewCell.swift
@@ -9,16 +9,25 @@
 import UIKit
 
 class DescriptionViewCell: UICollectionViewCell {
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
-        backgroundColor = .blue
+        backgroundColor = .white
     }
-    
+    let viewScheduleButton: UIButton = {
+        let button = UIButton()
+        button.layer.backgroundColor = UIColor.white.cgColor
+        button.setTitle("View Schedule", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.mainBlue, for: .normal)
+        return button
+    }()
+
     
     func setupViews() {
+        addSubview(viewScheduleButton)
         
+        viewScheduleButton.anchor(top: self.topAnchor, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: self.trailingAnchor)
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
