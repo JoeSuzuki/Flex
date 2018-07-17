a7e4d3757d2cdd7c8a31d6730f2d1a2778f9efa7
JoeSuzuki <joesuzuki516@gmail.com>
2018-07-17 19:43:18 -0400

    Search bar added to get info from map

--- Flex/Views/CreateGroupCells/CreateGroupCells.swift
+++ Flex/Views/CreateGroupCells/CreateGroupCells.swift
@@ -19,12 +19,18 @@ class GroupLocationCell: UICollectionViewCell {
         let map = MKMapView()
         return map
     }()
+    let searchBar: UISearchBar = {
+        let bar = UISearchBar()
+        return bar
+    }()
     
     func setupViews() {
         backgroundColor = .white
+        addSubview(searchBar)
         addSubview(mapView)
-        
-        mapView.anchor(top: self.topAnchor, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: self.trailingAnchor)
+
+        searchBar.anchor(top: self.topAnchor, leading: self.leadingAnchor, bottom: mapView.topAnchor, trailing: self.trailingAnchor)
+        mapView.anchor(top: searchBar.bottomAnchor, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: self.trailingAnchor)
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
