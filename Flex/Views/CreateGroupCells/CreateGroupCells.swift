a41c6cb7d12b2967842d153c330bc78ebee2a32e
JoeSuzuki <joesuzuki516@gmail.com>
2018-07-17 19:44:57 -0400

    New file for map cell

--- Flex/Views/CreateGroupCells/CreateGroupCells.swift
+++ Flex/Views/CreateGroupCells/CreateGroupCells.swift
@@ -7,35 +7,7 @@
 //
 
 import UIKit
-import MapKit
 
-class GroupLocationCell: UICollectionViewCell {
-    // Location
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupViews()
-    }
-    let mapView: MKMapView = {
-        let map = MKMapView()
-        return map
-    }()
-    let searchBar: UISearchBar = {
-        let bar = UISearchBar()
-        return bar
-    }()
-    
-    func setupViews() {
-        backgroundColor = .white
-        addSubview(searchBar)
-        addSubview(mapView)
-
-        searchBar.anchor(top: self.topAnchor, leading: self.leadingAnchor, bottom: mapView.topAnchor, trailing: self.trailingAnchor)
-        mapView.anchor(top: searchBar.bottomAnchor, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: self.trailingAnchor)
-    }
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
 class GroupProfilePictureCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
---
+++ Flex/Views/CreateGroupCells/GroupLocationCell.swift
@@ -0,0 +1,38 @@
+//
+//  GroupLocationCell.swift
+//  Flex
+//
+//  Created by Joe Suzuki on 7/17/18.
+//  Copyright © 2018 Joe Suzuki. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class GroupLocationCell: UICollectionViewCell {
+    // Location
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    let mapView: MKMapView = {
+        let map = MKMapView()
+        return map
+    }()
+    let searchBar: UISearchBar = {
+        let bar = UISearchBar()
+        return bar
+    }()
+    
+    func setupViews() {
+        backgroundColor = .white
+        addSubview(searchBar)
+        addSubview(mapView)
+        
+        searchBar.anchor(top: self.topAnchor, leading: self.leadingAnchor, bottom: mapView.topAnchor, trailing: self.trailingAnchor)
+        mapView.anchor(top: searchBar.bottomAnchor, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: self.trailingAnchor)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
